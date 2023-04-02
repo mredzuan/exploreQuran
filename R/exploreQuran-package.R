762451b94365ed30693141b5ba011e0d105b157c
redz <redzu.m@gmail.com>
2023-04-02 09:09:19 +0800

    Auto generated doc

---
+++ R/exploreQuran-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom tidytext unnest_tokens
+#' @importFrom wordcloud wordcloud
+## usethis namespace: end
+NULL
