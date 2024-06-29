ab699afb9a50077cde9dde9f9eef0cd0f656764f
redz <redzu.m@gmail.com>
2024-06-29 15:53:30 +0800

    Cont chap 2

--- R/analysis/Chap_2_1_QA.R
+++ R/analysis/Chap_2_1_QA.R
@@ -4,6 +4,7 @@ library(quRan)
 library(tidyverse)
 library(tidytext)
 library(ggplot2)
+library(dplyr)
 
 #wordcloud
 library(wordcloud)
@@ -18,7 +19,14 @@ qur_trans_ysf <- quran_en_yusufali
 
 #Unnest token-------
 
+
+## Sahih-----
 qur_trans_sahih_tkn <- quran_en_sahih |> 
+  unnest_tokens(word, text)
+
+
+## Yusof-----
+qur_trans_ysf_tkn <- quran_en_yusufali |> 
   unnest_tokens(word, text)
 
 
@@ -34,4 +42,58 @@ qur_trans_sahih_tkn |> count(word) |>
 
 #Document frequency------
 
+## Sahih-----
+wc_qur_trans_sahih_tk <-  qur_trans_sahih_tkn |> 
+  count(surah_title_en, word, sort = TRUE)
+
+sum_wc_trans_sahis_tk <- wc_qur_trans_sahih_tk |>  
+  group_by(surah_title_en) |> 
+  summarize(total = sum(n))
+
+wc_qur_trans_sahih_tk <- wc_qur_trans_sahih_tk |> 
+  left_join(sum_wc_trans_sahis_tk, by = "surah_title_en") |> 
+  mutate(tf = n/total)
+
+
+## Yusof Ali---------
+
+wc_qur_trans_ysf_tk <-  qur_trans_ysf_tkn |> 
+  count(surah_title_en, word, sort = TRUE)
+
+sum_wc_trans_ysf_tk <- wc_qur_trans_ysf_tk |>  
+  group_by(surah_title_en) |> 
+  summarize(total = sum(n))
+
+wc_qur_trans_ysf_tk <- wc_qur_trans_ysf_tk |> 
+  left_join(sum_wc_trans_ysf_tk, by = "surah_title_en") |> 
+  mutate(tf = n/total)
+
+#Plotting tf-----------
+
+last6_surahs = c("An-Naas", "Al-Falaq", "Al-Ikhlaas", "Al-Masad",
+                 "An-Nasr", "Al-Kaafiroon")
+
+
+## Sahih-----
+wc_qur_trans_sahih_tk |> 
+  filter(surah_title_en %in% last6_surahs) |> 
+  ggplot(aes(tf, fill = surah_title_en)) + 
+    geom_histogram() +
+    facet_wrap(~surah_title_en, ncol = 2) +
+    theme_bw() + 
+    ggtitle("Sahih Translation")
+
+
+## Yusof ------
+
+wc_qur_trans_ysf_tk |> 
+  filter(surah_title_en %in% last6_surahs) |> 
+  ggplot(aes(tf, fill = surah_title_en)) + 
+  geom_histogram() +
+  facet_wrap(~surah_title_en, ncol = 2) +
+  theme_bw() +
+  ggtitle("Yusof Ali Translation")
+
+
+### Cont page 45
 
